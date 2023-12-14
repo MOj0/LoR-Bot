90450eaa199359610ce5c75bcd802c2a55c66997
bomoy <153855381+bomoy@users.noreply.github.com>
2023-12-14 19:08:01 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -89,10 +89,10 @@ class Bot:
         sleep(4)
         continue_btn_pos = (self.window_x + 0.66 * self.window_width, self.window_y + self.window_height * 0.90)
         second_continue_btn_pos = (self.window_x + 0.73854 * self.window_width, self.window_y + self.window_height * 0.92222)
-        for _ in range(16):
-            self.mouse_handler.click(continue_btn_pos)
-            self.mouse_handler.click(second_continue_btn_pos)
-            sleep(1.5)
+        self.mouse_handler.click(continue_btn_pos)
+        self.mouse_handler.click(continue_btn_pos)
+        sleep(1.5)
+        self.mouse_handler.click(second_continue_btn_pos)
         sleep(1)
 
     def is_state_playable(self) -> bool:
